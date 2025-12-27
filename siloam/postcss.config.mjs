f85edb13caca5f5401f89e9bd702d9314d40a5fc
Henry "TJ" Chen <127168121+LatinScribe@users.noreply.github.com>
2025-12-27 13:35:18 -0500

    Update postcss.config.mjs

--- siloam/postcss.config.mjs
+++ siloam/postcss.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('postcss-load-config').Config} */
+import tailwindcss from 'tailwindcss';
+
 const config = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: tailwindcss(),
   },
 };
 
